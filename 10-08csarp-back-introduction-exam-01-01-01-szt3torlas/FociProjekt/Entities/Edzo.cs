b8fcc59d042f06f98e1a764fa59cf19044d47bad
szt3torlas <szt3torlas@vasvari.org>
2025-02-03 11:46:23 +0100

    Commit2;

--- 10-08csarp-back-introduction-exam-01-01-01-szt3torlas/FociProjekt/Entities/Edzo.cs
+++ 10-08csarp-back-introduction-exam-01-01-01-szt3torlas/FociProjekt/Entities/Edzo.cs
@@ -4,6 +4,6 @@
     {
         public string Nev { get; set; } = string.Empty;
         public int Fizetes { get; set; }
-        public FociKlub? FociKlub { get; set; }
+        public FociKlub? FociKlub { get; set; }///
     }
 }
